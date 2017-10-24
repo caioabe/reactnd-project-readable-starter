9c7033cd30a152ea3abd8fd18c86edd53c085631
Caio Abe <caio.abe@estilo7.com.br>
2017-10-23 23:16:19 -0200

    Use react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        Hi there!
+      <div className="app">
+        <Route exact path='/' render={() => (
+          'empty'
+        )} />
       </div>
-    );
+    )
   }
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom'
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const component = (
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+)
+
+ReactDOM.render(component, document.getElementById('root'))
