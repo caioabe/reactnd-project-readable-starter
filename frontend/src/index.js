a676a0b0a7e118676634bda69ed8740e84f8dadb
Caio Abe <caio.abe@estilo7.com.br>
2017-10-25 08:42:41 -0200

    Config index.js to use middlewares and create the store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter } from 'react-router-dom';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 
-const component = (
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-)
+import App from './App';
+import rootReducer from './config/root-reducer';
+import registerServiceWorker from './registerServiceWorker';
+import './index.css';
 
-ReactDOM.render(component, document.getElementById('root'))
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(logger, thunk)));
+
+const component = (
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+);
+
+ReactDOM.render(component, document.getElementById('root'));
 registerServiceWorker();
