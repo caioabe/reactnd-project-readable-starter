34ca967d6e3e58af363b8ee5df42539edde23eb4
Caio Abe <caio.abe@estilo7.com.br>
2017-10-25 23:52:00 -0200

    Change middleware order

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -17,7 +17,7 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   rootReducer,
-  composeEnhancers(applyMiddleware(logger, thunk)));
+  composeEnhancers(applyMiddleware(thunk, logger)));
 
 const component = (
   <Provider store={store}>
