90206fa189b2a1760c9c4cb53a97f28099b8a634
Caio Abe <caio.abe@estilo7.com.br>
2017-10-27 07:48:36 -0200

    Use Nav component in category-menu component

--- frontend/src/components/category-menu/index.js
+++ frontend/src/components/category-menu/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Nav } from 'react-bootstrap';
+
 import { CategoryLink } from '../';
 
 function renderCategoryLink(category) {
   return <CategoryLink category={category} key={`${category.name}-link`} />;
 }
 
-const CategoryMenu = ({ categories }) => <ul>{categories.map(renderCategoryLink)}</ul>;
+const CategoryMenu = ({ categories }) => (
+  <Nav bsStyle="pills">
+    {categories.map(renderCategoryLink)}
+  </Nav>
+);
 
 export { CategoryMenu };
