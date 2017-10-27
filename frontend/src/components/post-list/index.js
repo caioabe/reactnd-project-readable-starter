f073e61da57365f3bfc6d61af499513437ac7ea4
Caio Abe <caio.abe@estilo7.com.br>
2017-10-27 07:49:06 -0200

    Use Row component in post-list component

--- frontend/src/components/post-list/index.js
+++ frontend/src/components/post-list/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Row } from 'react-bootstrap';
+
 import { PostItem } from '../';
 
 function renderPostItem(post) {
   return <PostItem post={post} key={`${post.id}-menu`} />;
 }
 
-const PostList = ({ posts }) => posts.map(renderPostItem);
+const PostList = ({ posts }) => (
+  <Row>
+    { posts.map(renderPostItem) }
+  </Row>
+);
 
 export { PostList };
