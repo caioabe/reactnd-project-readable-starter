262cefba0816c1660210049586eb2314bf22d8ff
Caio Abe <caio.abe@estilo7.com.br>
2017-10-27 07:48:23 -0200

    Use LinkContainer and NavItem in category-link component

--- frontend/src/components/category-link/index.js
+++ frontend/src/components/category-link/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { LinkContainer } from 'react-router-bootstrap';
+import { NavItem } from 'react-bootstrap';
 
 const CategoryLink = ({ category }) => (
-  <li>
-    <Link to={`/category/${category.path}/posts`} children={category.name}/>
-  </li>
+  <LinkContainer to={`/category/${category.path}/posts`}>
+    <NavItem>{category.name}</NavItem>
+  </LinkContainer>
 );
 
 export { CategoryLink };
