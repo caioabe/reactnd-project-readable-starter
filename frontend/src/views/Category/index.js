f8c14a229a36f8204282b6f6d50433bd7286cba3
Caio Abe <caio.abe@estilo7.com.br>
2017-11-02 08:54:43 -0200

    Fix bug in Category view

--- frontend/src/views/Category/index.js
+++ frontend/src/views/Category/index.js
@@ -1,37 +1,44 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 import { PostsByCategory } from '../';
 import { CategoryMenu } from '../../components';
 import { getCategories } from '../../modules/categories';
+import { getPosts } from '../../modules/posts';
 
 class Category extends Component {
   componentWillMount() {
+    if (this.props.posts.length === 0) {
+      this.props.getPosts();
+    }
+
     this.props.getCategories();
   }
 
   render() {
-    const { url } = this.props.match;
     const { categories } = this.props;
 
     return (
       <div>
         <h2>Category</h2>
         <CategoryMenu categories={categories} />
-        <Route path={`${url}/:categoryId/posts`} component={PostsByCategory}/>
-        <Route exact path={url} render={() => (
-          <h3>Please select a topic.</h3>
-        )}/>
+        <Switch>
+          <Route exact path='/category' render={() => (
+            <h3>Please select a topic.</h3>
+          )}/>
+          <Route path={'/category/:categoryId'} component={PostsByCategory}/>
+        </Switch>
       </div>
     );
   }
 }
 
-const mapDispatchToProps = { getCategories };
-const mapStateToProps = ({ categories }) => (
+const mapDispatchToProps = { getPosts, getCategories };
+const mapStateToProps = ({ posts, categories }) => (
   {
+    posts,
     categories,
   }
 );
@@ -41,9 +48,11 @@ const connectedComponent = connect(mapStateToProps, mapDispatchToProps)(Category
 export { connectedComponent as Category };
 
 Category.propTypes = {
+  posts: PropTypes.array,
   categories: PropTypes.array,
 };
 
 Category.defaultProps = {
+  posts: [],
   categories: [],
 };
