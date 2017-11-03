37dc09adf030add2bcc2b80d171a1fe204b595f7
Caio Abe <caio.abe@estilo7.com.br>
2017-11-03 11:30:05 -0200

    Fix new post link

--- frontend/src/views/Main/index.js
+++ frontend/src/views/Main/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Grid } from 'react-bootstrap';
 import { Route, Switch } from 'react-router-dom';
-import { Root, Category, Post, Comment } from '../';
+import { Root, Category, Post, Comment, PostForm } from '../';
 
 const Main = () => (
   <Grid>
@@ -10,6 +10,7 @@ const Main = () => (
       <Route path="/category" component={Category}/>
       <Route path="/post" component={Post}/>
       <Route path="/comment" component={Comment}/>
+      <Route path="/new-post" component={PostForm}/>
     </Switch>
   </Grid>
 );
