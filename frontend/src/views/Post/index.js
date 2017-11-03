0f4430747baa6e0edd253c11142a449265c82119
Caio Abe <caio.abe@estilo7.com.br>
2017-11-03 13:14:17 -0200

    Add view for post root

--- frontend/src/views/Post/index.js
+++ frontend/src/views/Post/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { LinkContainer } from 'react-router-bootstrap';
+import { Button } from 'react-bootstrap';
 
 import { PostDetail, PostForm } from '../';
 
@@ -12,6 +14,11 @@ class Post extends Component {
       <div>
         <h2>Post</h2>
         <Switch>
+          <Route exact path={`${url}`} render={() => (
+            <LinkContainer exact to={'/new-post'}>
+              <Button bsStyle="primary">Create New Post</Button>
+            </LinkContainer>
+          )}/>
           <Route exact path={`${url}/:postId`} component={PostDetail}/>
           <Route path={`${url}/:postId/edit`} component={PostForm}/>
         </Switch>
