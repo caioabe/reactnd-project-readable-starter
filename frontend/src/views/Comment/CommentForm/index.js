05fa0e7d8df6e66177071169c174ab1ad8a274d0
Caio Abe <caio.abe@estilo7.com.br>
2017-11-03 19:34:56 -0200

    Create comment form

--- frontend/src/views/Comment/CommentForm/index.js
+++ frontend/src/views/Comment/CommentForm/index.js
@@ -4,30 +4,89 @@ import { connect } from 'react-redux';
 import { FormGroup, ControlLabel, FormControl, Button } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 
+import { updateComment, createComment } from '../../../modules/comments';
+
 class CommentForm extends Component {
   constructor(props) {
     super(props);
 
     this.handleSave = this.handleSave.bind(this);
+    this.handleCreate = this.handleCreate.bind(this);
+    this.renderButton = this.renderButton.bind(this);
+    this.handleInput = this.handleInput.bind(this);
 
     this.state = {
       commentId: '',
+      parentId: '',
+      isNew: false,
     };
   }
 
   componentWillMount() {
     this.setState({
       commentId: this.props.match.params.commentId,
+      parentId: this.props.match.params.postId,
+      isNew: this.props.match.path.indexOf('/new-comment') >= 0,
     });
   }
 
   handleSave() {
-    this.props.savePost(this.state.commentId);
+    const { comments } = this.props;
+    const { commentId } = this.props.match.params;
+    const comment = _.chain(comments)
+      .values()
+      .head()
+      .find(c => c.id === commentId)
+      .value() || [];
+
+    this.props.updateComment({
+      id: this.state.commentId,
+      author: comment.author,
+      body: this.state.body,
+      parentId: comment.parentId,
+    });
+    this.props.history.push(`/comment/${this.state.commentId}`);
+  }
+
+  handleCreate() {
+    this.props.createComment({
+      author: this.state.author,
+      body: this.state.body,
+      parentId: this.state.parentId,
+    });
+    this.props.history.push(`/post/${this.state.parentId}`);
+  }
+
+  renderButton() {
+    let button;
+
+    if (this.state.isNew) {
+      button = (
+        <Button bsStyle="primary" onClick={this.handleCreate}>
+          Create comment
+        </Button>
+      );
+    } else {
+      button = (
+        <Button bsStyle="success" onClick={this.handleSave}>
+          Save editing
+        </Button>
+      );
+    }
+
+    return button;
+  }
+
+  handleInput(name) {
+    return (event) => {
+      this.setState({ [name]: event.target.value });
+    };
   }
 
   render() {
     const { comments } = this.props;
     const { commentId } = this.props.match.params;
+    const { isNew } = this.state;
     const comment = _.chain(comments)
       .values()
       .head()
@@ -39,18 +98,29 @@ class CommentForm extends Component {
         <form>
           <FormGroup>
             <ControlLabel>Body:</ControlLabel>
-            <FormControl componentClass="textarea" placeholder="Describe..." defaultValue={comment.body}/>
+            <FormControl
+              componentClass="textarea"
+              placeholder="Describe..."
+              defaultValue={comment.body}
+              onChange={this.handleInput('body')}/>
+          </FormGroup>
+          <FormGroup>
+            <ControlLabel>Author:</ControlLabel>
+            <FormControl
+              disabled={!isNew}
+              componentClass="input"
+              placeholder="Describe..."
+              defaultValue={comment.author}
+              onChange={this.handleInput('author')}/>
           </FormGroup>
         </form>
-        <Button bsStyle="success" onClick={this.handleSave}>
-          Save editing
-        </Button>
+        {this.renderButton()}
       </div>
     );
   }
 }
 
-const mapDispatchToProps = { };
+const mapDispatchToProps = { updateComment, createComment };
 const mapStateToProps = ({ comments }) => (
   {
     comments,
